7d311684d48559a9889f175d52c8cc1341cfe00e
Fırat Selamet <firatselamet34@gmail.com>
2026-09-23 14:13:21 +0300

    ilk odev eklendi

---
+++ Odev1/Soru1.c
@@ -0,0 +1,43 @@
+/*  Soru 1
+C programlama dilinde 10 elemanlı bir tamsayı dizisi tanımlayınız. Kullanıcıdan dizinin 10
+elemanını alınız ve daha sonra bu elemanları ekrana yazdırınız.
+Programın:
+1. C kodunu yazınız.
+2. T(n) zaman maliyetini hesaplayınız.
+3. O(n) zaman karmaşıklığını bulunuz.
+4. Programın S(n) alan karmaşıklığını bulunuz.
+Not: n, dizideki eleman sayısını ifade etmektedir.  */    
+
+#include <stdio.h>
+
+int main()
+{
+    int dizi[10];
+    int i;
+
+    printf("Lutfen 10 adet tam sayi giriniz: \n");
+    for (i = 0; i<10; i++)
+    {
+        scanf("%d", &dizi[i]);
+    }
+
+    printf("Girdiginiz sayilar gosteriliyor...\n");
+    for ( i=0 ; i< 10 ; i++)
+    {
+        printf("%d\n", dizi[i]);
+    }
+
+    return 0;
+}
+/*  S(n) = n + 1  Çünkü dizinin boyutu n ve diğer değişkenimizin olan i'nin maaliyeti 1'dir.
+
+        Big O gösterimi ile S(n) = O(n) olur. Çünkü dizinin boyutu n ve diğer değişkenimizin olan i'nin maaliyeti 1'dir.
+    Bu nedenle O(n) alan karmaşıklığına sahiptir.
+
+    T(n) = 6n + 7 Tek for döngüsü ile dizinin elemanlarını almak ve yazdırmak için n kez çalışır. Döngünün başında i'ye değer ataması sabit ve 1'dir.
+Döngü kontrolü n+1 kez çalışır. Döngüde i değerinin artması n kez çalışır. Döngüdeki scanf ve printf fonksiyonları n kez çalışır. İlk ve son for döngüsünün
+başında son olarak da 0 değeri döndürdüğümüz işlemlerin de maliyeti 1'dir. Bu nedenle T(n) = 6n + 7 olur.
+
+    O(n) zaman karmaşıklığına sahiptir. Çünkü T(n) = 6n + 7 fonksiyonunda n'nin katsayısı 6'dır ve diğer terimler sabittir.
+Bu nedenle O(n) zaman karmaşıklığına sahiptir.
+*/
---
+++ Odev1/Soru2.c
@@ -0,0 +1,45 @@
+/*  Soru 2:
+Kullanıcıdan bir tam sayı alın ve bu sayının bir Palindrom Sayı olup olmadığını bulan C
+programını yazın.   */
+
+#include <stdio.h>
+
+int main()
+{
+    int sayi;
+    int kalan = 0;
+    int ters = 0;
+
+    printf("Lutfen bir sayi giriniz: \n");
+    scanf("%d", &sayi);
+
+    int test = sayi;
+
+    while (sayi > 0)
+    {
+        kalan = sayi % 10;
+        ters = ters * 10 + kalan;
+        sayi /= 10;
+    }  
+
+    if ( test == ters)
+        printf("Girdiginiz sayi palindromdur.\n");
+    else
+        printf("Girdiginiz sayi palindrom degildir.\n");
+
+return 0;
+}
+/*  S(n) = Toplam 4 değişkenimiz olduğundan maliyetimiz 4'tür.
+
+        Big O gösterimi ile S(n) = O(1) olur. Çünkü toplam 4 değişkenimiz olduğundan maliyetimiz 4'tür.
+    Bu nedenle O(1) alan karmaşıklığına sahiptir.
+
+    T(n) = İki tane değişkene tanımlandığı anda değer verildi iki diğer değişkene tanımlandıktan sonra değer verildi burada maliyet 4'tür.
+While döngüsünden hemen önce iki tane fonksiyon çalıştırıldı burada maliyet 2'dir. While döngüsü girilen sayının basamak sayısı kadar çalıştığından
+her döngüde log 10 tabanı çarpımı tane işlem yapılır, burada maliyet 3log₁₀(n) olur.While döngüsündeki döngü kontrolü n+1 kez çalışır ve maliyeti log₁₀(n)+1'dir.
+While döngüsünden sonra if else kontrolü yapılır ve maliyeti 1'dir ve koşulların herhangi birinin içine girildiği takdirde çalışacak bir adet fonksiyon daha
+mevcuttur ve maliyeti 1'dir. Son olarak 0 değeri döndürdüğümüz işlemin de maliyeti 1'dir. Bu nedenle T(n) = 4log₁₀(n) + 10 olur.
+
+    O(log n) zaman karmaşıklığına sahiptir. Çünkü T(n) = 4log₁₀(n) + 10 fonksiyonunda log₁₀(n)'nin katsayısı 4'dir ve diğer terimler sabittir.
+Bu nedenle O(log n) zaman karmaşıklığına sahiptir.
+*/
