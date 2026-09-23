d644094d8710509fad79b1223064eff4cafc51fe
Fırat Selamet <firatselamet34@gmail.com>
2026-09-23 18:10:42 +0300

    Soru 1 ve Soru 2 icin C# cozumleri eklendi

---
+++ C-Sharp/Odev-1/Soru1.cs
@@ -0,0 +1,48 @@
+/*  Soru 1
+C programlama dilinde 10 elemanlı bir tamsayı dizisi tanımlayınız. Kullanıcıdan dizinin 10
+elemanını alınız ve daha sonra bu elemanları ekrana yazdırınız.
+Programın:
+1. C kodunu yazınız.
+2. T(n) zaman maliyetini hesaplayınız.
+3. O(n) zaman karmaşıklığını bulunuz.
+4. Programın S(n) alan karmaşıklığını bulunuz.
+Not: n, dizideki eleman sayısını ifade etmektedir.  */
+
+using System;
+
+namespace Odev_1
+{
+    class Soru1
+    {
+        static void Main(string[] args)
+        {
+        int[] dizi = new int[10];
+        int i;
+
+        Console.WriteLine("Lutfen 10 adet tam sayi giriniz: ");
+            for (i = 0; i < 10; i++)
+            {
+                dizi[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+        Console.WriteLine("Girdiginiz sayilar gosteriliyor...");
+            for (i = 0; i < 10; i++)
+            {
+                Console.WriteLine(dizi[i]);
+            }
+        }
+    }
+}
+
+/*  S(n) = n + 1  Çünkü dizinin boyutu n ve diğer değişkenimizin olan i'nin maaliyeti 1'dir.
+
+        Big O gösterimi ile S(n) = O(n) olur. Çünkü dizinin boyutu n ve diğer değişkenimizin olan i'nin maaliyeti 1'dir.
+    Bu nedenle O(n) alan karmaşıklığına sahiptir.
+
+    T(n) = 6n + 7 Tek for döngüsü ile dizinin elemanlarını almak ve yazdırmak için n kez çalışır. Döngünün başında i'ye değer ataması sabit ve 1'dir.
+Döngü kontrolü n+1 kez çalışır. Döngüde i değerinin artması n kez çalışır. Döngüdeki scanf ve printf fonksiyonları n kez çalışır. İlk ve son for döngüsünün
+başında son olarak da 0 değeri döndürdüğümüz işlemlerin de maliyeti 1'dir. Bu nedenle T(n) = 6n + 7 olur.
+
+    O(n) zaman karmaşıklığına sahiptir. Çünkü T(n) = 6n + 7 fonksiyonunda n'nin katsayısı 6'dır ve diğer terimler sabittir.
+Bu nedenle O(n) zaman karmaşıklığına sahiptir.
+*/
